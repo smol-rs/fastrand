f939572a41fc01f03dd239d72c52fdeba3456274
John Nunley <jtnunley01@gmail.com>
2023-03-03 18:49:04 -0800

    docs: Add indicators for methods that are std-only (#53)
    * docs: Add std-only indications
    
    * Module-level doc
    
    * fmt

--- src/lib.rs
+++ src/lib.rs
@@ -72,6 +72,7 @@
 //!   the [`with_seed`](Rng::with_seed) method.
 
 #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 
@@ -79,6 +80,7 @@ use core::convert::{TryFrom, TryInto};
 use core::ops::{Bound, RangeBounds};
 
 #[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
 mod global_rng;
 
 #[cfg(feature = "std")]
