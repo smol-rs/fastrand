44c8a9b5b83d6595a178d7ede852afaa6baef9b2
John Nunley <jtnunley01@gmail.com>
2023-07-16 21:47:17 -0700

    Add smol-rs logo

--- src/lib.rs
+++ src/lib.rs
@@ -98,6 +98,12 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
+#![doc(
+    html_favicon_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
+)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
+)]
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
